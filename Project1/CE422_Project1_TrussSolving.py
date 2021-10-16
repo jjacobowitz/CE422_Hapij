581c56c3450cda0afb0e4c6c222c17d1d0d42d70
jjacobowitz <57004711+jjacobowitz@users.noreply.github.com>
2021-10-16 19:23:10 -0400

    Multiple improvements
    * clarified some comments
    * added yield stress variable
    * corrected H equation
    * improved "rows" variable calculation
    * calculate FOS
    * print member load, stress, and strain results
    * print reaction forces

--- Project1/CE422_Project1_TrussSolving.py
+++ Project1/CE422_Project1_TrussSolving.py
@@ -13,15 +13,19 @@ theta = np.deg2rad(60)
 s = np.sin(theta)
 c = np.cos(theta)
 
-# member properties
-length = 5          # m; member lengths
-area = 0.0254**2    # m^2; member cross-sectional area
-Youngs = 200e9      # Pa; Young's modulus of steel
+# member geometric properties
+length = 5              # m; member lengths
+area = 0.0254**2        # m^2; member cross-sectional area
+
+# material properties; shorturl.at/boCWY
+Youngs = 200e9          # Pa; Young's modulus of steel
+yield_stress = 350e6    # Pa; yield stress of steel
 
 # applied loads
-P1 = 10000          # N; force 1 (vertical force on node B)
-P2 = 25000          # N; force 2 (vertical force on node L)
+P1 = 10000          # N; force 1 (negative vertical force on node B)
+P2 = 25000          # N; force 2 (negative vertical force on node L)
 
+# nodal x and y equations
 A = {"x":{"AB":1, "AC":c, "Ax":1},
      "y":{"AC":-s, "Ay":-1}}
 B = {"x":{"AB":-1, "BC":-c, "BD":c},
@@ -37,7 +41,7 @@ F = {"x":{"DF":-c, "EF":-1, "FG":-c, "FH":c, "FI":1},
 G = {"x":{"EG":-c, "FG":c, "GH":1},
      "y":{"EG":s, "FG":s}}
 H = {"x":{"FH":-c, "GH":-1, "HI":c, "HJ":1},
-     "y":{"FH":s, "HI":-s, "Hy":1}}
+     "y":{"FH":s, "HI":s, "Hy":1}}
 I = {"x":{"FI":-1, "HI":-c, "IJ":c, "IK":1},
      "y":{"HI":-s, "IJ":-s}}
 J = {"x":{"HJ":-1, "IJ":-c, "JK":c, "JL":1},
@@ -59,12 +63,12 @@ members = ["AB", "AC", "BC", "BD", "CD", "CE", "DE", "DF", "EF", "EG", "FG",
 mat = np.zeros((24,24))
 
 # Fill "A" matrix with statics equations
-row = -2
+row = 0
 for node in nodes:
-    row += 2        # every 2 rows is a new node; each row for x and y equation
     for indx, member in enumerate(members):
         mat[row][indx] = node["x"].get(member, 0)
         mat[row+1][indx] = node["y"].get(member, 0)
+    row += 2        # every 2 rows is a new node; each row for x and y equation
         
 # "b" matrix for Ax=b; this is the matrix of loads
 P = np.zeros((24,1))
@@ -75,10 +79,29 @@ P[23] = P2
 x = np.linalg.solve(mat, P)
 
 # stress is force over area (don't apply to reactions)
-stress = x[:-3]/area
+stresses = x[:-3]/area
 
-# sigma = youngs*strain
-strain = stress/Youngs
+# stress = Youngs*strain -> strain = stress/Youngs
+strains = stresses/Youngs
 
-# check if any members fail
-print(np.any(np.abs(stress) > 350e6))
+# check if any members fail; show the FOS of the highest stressed member
+print(np.max(np.abs(stresses)) > yield_stress)
+print("FOS", yield_stress/np.max(np.abs(stresses)))
+
+# print the member results
+results = dict()
+for member, load, stress, strain in zip(members, x, stresses, strains, ):
+    print(member)
+    print(f"\t Load: {load[0]:.3e}N")
+    print(f"\t Stress: {stress[0]:.3e}Pa")
+    print(f"\t Strain: {strain[0]:.3e}")
+    results[member] = {"load [N]":load[0],
+                       "stress [Pa]":stress[0],
+                       "strain":strain[0]}
+
+# print the reactions
+reactions = dict()
+for reaction, load in zip(members[-3:], x[-3:]):
+    print(f"{reaction}: {load[0]:.3f}N")
+    reactions[reaction] = load[0]
+    
